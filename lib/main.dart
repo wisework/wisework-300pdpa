bab7ada6d171da1da4e20dc53b6822e3b1b132ae
Peerapat Kaosoon <Invisible.P4979@gmail.com>
2023-09-08 14:06:41 +0700

    Add firebase plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pdpa/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
