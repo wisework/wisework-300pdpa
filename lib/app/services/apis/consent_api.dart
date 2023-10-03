cd1490b361a717f57fab32c0668a908c3756b8dc
Panyawut Suton <92622255+BossPanyawut@users.noreply.github.com>
2023-10-03 14:33:40 +0700

    fix: Add API and repository of consent

---
+++ lib/app/services/apis/consent_api.dart
@@ -0,0 +1,168 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pdpa/app/data/models/consent_management/consent_form_model.dart';
+import 'package:pdpa/app/data/models/consent_management/consent_theme_model.dart';
+import 'package:pdpa/app/data/models/consent_management/user_consent_model.dart';
+
+class ConsentApi{
+  const ConsentApi(this._firestore);
+
+  final FirebaseFirestore _firestore;
+
+//? Consenst Form
+  Future<List<ConsentFormModel>> getConsentForms(String companyId) async {
+    final result =
+        await _firestore.collection('Companies/$companyId/ConsentForms').get();
+
+    List<ConsentFormModel> consentForms = [];
+    for (var document in result.docs) {
+      consentForms.add(ConsentFormModel.fromDocument(document));
+    }
+
+    return consentForms;
+  }
+
+  Future<ConsentFormModel?> getConsentFormById(
+    String consentFormsId,
+    String companyId,
+  ) async {
+    final result = await _firestore
+        .collection('Companies/$companyId/ConsentForms')
+        .doc(consentFormsId)
+        .get();
+
+    if (!result.exists) return null;
+    return ConsentFormModel.fromDocument(result);
+  }
+
+  Future<ConsentFormModel> createConsentForm(
+    ConsentFormModel consentForm,
+    String companyId,
+  ) async {
+    final ref = _firestore.collection('Companies/$companyId/ConsentForms').doc();
+    final created = consentForm.copyWith(consentFormId: ref.id);
+
+    await ref.set(created.toMap());
+
+    return created;
+  }
+
+  Future<void> updateConsentForm(
+    ConsentFormModel consentForm,
+    String companyId,
+  ) async {
+    await _firestore
+        .collection('Companies/$companyId/ConsentForms')
+        .doc(consentForm.consentFormId)
+        .set(consentForm.toMap());
+  }
+
+
+  //? Consenst Theme
+ Future<List<ConsentThemeModel>> getConsentThemes(String companyId) async {
+    final result =
+        await _firestore.collection('Companies/$companyId/ConsentThemes').get();
+
+    List<ConsentThemeModel> consentThemes = [];
+    for (var document in result.docs) {
+      consentThemes.add(ConsentThemeModel.fromDocument(document));
+    }
+
+    return consentThemes;
+  }
+
+  Future<ConsentThemeModel?> getConsentThemeById(
+    String consentThemeId,
+    String companyId,
+  ) async {
+    final result = await _firestore
+        .collection('Companies/$companyId/ConsentThemes')
+        .doc(consentThemeId)
+        .get();
+
+    if (!result.exists) return null;
+    return ConsentThemeModel.fromDocument(result);
+  }
+
+  Future<ConsentThemeModel> createConsentTheme(
+    ConsentThemeModel consentTheme,
+    String companyId,
+  ) async {
+    final ref = _firestore.collection('Companies/$companyId/ConsentThemes').doc();
+    final created = consentTheme.copyWith(consentThemeId: ref.id);
+
+    await ref.set(created.toMap());
+
+    return created;
+  }
+
+  Future<void> updateConsentTheme(
+    ConsentThemeModel consentTheme,
+    String companyId,
+  ) async {
+    await _firestore
+        .collection('Companies/$companyId/ConsentThemes')
+        .doc(consentTheme.consentThemeId)
+        .set(consentTheme.toMap());
+  }
+
+  Future<void> deleteConsentTheme(
+    String consentThemeId,
+    String companyId,
+  ) async {
+    if (consentThemeId.isNotEmpty) {
+      await _firestore
+          .collection('Companies/$companyId/ConsentThemes')
+          .doc(consentThemeId)
+          .delete();
+    }
+  }
+
+  //? User consent
+Future<List<UserConsentModel>> getUserConsents(String companyId) async {
+    final result =
+        await _firestore.collection('Companies/$companyId/UserConsents').get();
+
+    List<UserConsentModel> userConsents = [];
+    for (var document in result.docs) {
+      userConsents.add(UserConsentModel.fromDocument(document));
+    }
+
+    return userConsents;
+  }
+
+  Future<UserConsentModel?> getUserConsentById(
+    String userConsentId,
+    String companyId,
+  ) async {
+    final result = await _firestore
+        .collection('Companies/$companyId/UserConsents')
+        .doc(userConsentId)
+        .get();
+
+    if (!result.exists) return null;
+    return UserConsentModel.fromDocument(result);
+  }
+
+  Future<UserConsentModel> createUserConsent(
+    UserConsentModel userConsent,
+    String companyId,
+  ) async {
+    final ref = _firestore.collection('Companies/$companyId/UserConsents').doc();
+    final created = userConsent.copyWith(consentFormId: ref.id);
+
+    await ref.set(created.toMap());
+
+    return created;
+  }
+
+  Future<void> updateUserConsent(
+    UserConsentModel userConsent,
+    String companyId,
+  ) async {
+    await _firestore
+        .collection('Companies/$companyId/UserConsents')
+        .doc(userConsent.consentFormId)
+        .set(userConsent.toMap());
+  }
+
+}
