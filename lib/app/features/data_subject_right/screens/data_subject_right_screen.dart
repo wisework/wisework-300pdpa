6d17b59208df1383e375806dd6948b4e9dc4f47b
Gun Jiraphat <sriyuyongj@gmail.com>
2023-10-11 11:49:52 +0700

    fix: Appbar DSR

--- lib/app/features/data_subject_right/screens/data_subject_right_screen.dart
+++ lib/app/features/data_subject_right/screens/data_subject_right_screen.dart
@@ -1,7 +1,12 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:pdpa/app/config/config.dart';
 import 'package:pdpa/app/features/data_subject_right/routes/data_subject_right_route.dart';
+import 'package:pdpa/app/shared/drawers/pdpa_drawer.dart';
 import 'package:pdpa/app/shared/widgets/customs/custom_container.dart';
+import 'package:pdpa/app/shared/widgets/customs/custom_icon_button.dart';
 import 'package:pdpa/app/shared/widgets/templates/pdpa_app_bar.dart';
 
 class DSRScreen extends StatefulWidget {
@@ -12,20 +17,47 @@ class DSRScreen extends StatefulWidget {
 }
 
 class _DSRScreenState extends State<DSRScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const PdpaAppBar(
-        title: Text('DSR'),
+      key: _scaffoldKey,
+      appBar: PdpaAppBar(
+        leadingIcon: CustomIconButton(
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
+          icon: Ionicons.menu_outline,
+          iconColor: Theme.of(context).colorScheme.primary,
+          backgroundColor: Theme.of(context).colorScheme.onBackground,
+        ),
+        title: Text(
+          tr('app.features.datasubjectright'),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
       ),
-      body: const CustomContainer(
-        child: Text('TEST'),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: UiConfig.lineSpacing),
+            CustomContainer(
+              child: Text('TEST'),
+            ),
+            SizedBox(height: UiConfig.lineSpacing),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           context.push(DataSubjectRightRouter.start.path);
         },
         child: const Icon(Icons.add),
+      ),
+      drawer: PdpaDrawer(
+        onClosed: () {
+          _scaffoldKey.currentState?.closeDrawer();
+        },
       ),
     );
   }
